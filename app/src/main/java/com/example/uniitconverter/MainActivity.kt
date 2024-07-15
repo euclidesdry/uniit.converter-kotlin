1e481fb459fd00b31b8a1ca45a8c7f48d3bba10e
Euclides do Rosário <euclidesdry@gmail.com>
2024-07-15 22:34:30 +0100

    feat: adding Box, Icons and DropDownMenus

--- app/src/main/java/com/example/uniitconverter/MainActivity.kt
+++ app/src/main/java/com/example/uniitconverter/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.uniitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.uniitconverter.ui.theme.UniitConverterTheme
 
@@ -34,14 +36,19 @@ fun UnitConverter() {
         OutlinedTextField(value = "", onValueChange = {})
         
         Row {
-            val context = LocalContext.current
-
-            Button(onClick = { Toast
-                .makeText(context,
-                    "Thanks for clicking!",
-                    Toast.LENGTH_LONG).show() })
-            {
-                Text("Click Me!")
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+            }
+            Box {
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
             }
         }
         Text("Result:")
