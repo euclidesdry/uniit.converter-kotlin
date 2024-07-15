924c53fa52e96eaa493dbfb20dcf9a57fc8c7708
Euclides do Rosário <euclidesdry@gmail.com>
2024-07-15 22:27:24 +0100

    feat: adding button, onClick, and Toast

--- app/src/main/java/com/example/uniitconverter/MainActivity.kt
+++ app/src/main/java/com/example/uniitconverter/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.uniitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.uniitconverter.ui.theme.UniitConverterTheme
 
@@ -34,7 +34,15 @@ fun UnitConverter() {
         OutlinedTextField(value = "", onValueChange = {})
         
         Row {
+            val context = LocalContext.current
 
+            Button(onClick = { Toast
+                .makeText(context,
+                    "Thanks for clicking!",
+                    Toast.LENGTH_LONG).show() })
+            {
+                Text("Click Me!")
+            }
         }
         Text("Result:")
     }
