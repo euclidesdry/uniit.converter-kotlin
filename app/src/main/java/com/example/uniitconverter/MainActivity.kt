f21ba9d7f485d2c29663cf816679ca8217e03090
Euclides do Rosário <euclidesdry@gmail.com>
2024-07-15 21:46:17 +0100

    feat: starting project

---
+++ app/src/main/java/com/example/uniitconverter/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.uniitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.uniitconverter.ui.theme.UniitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UniitConverterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    UnitConverter(innerPadding)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UnitConverter(innerPadding: PaddingValues) {
+    Column {
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {})
+        
+        Row {
+
+        }
+    }
+}
+
